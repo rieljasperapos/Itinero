931911d7cba9d7bff9fcd797af44ddf3aa037052
Jasper <jasper.apos@gmail.com>
2024-10-23 15:45:02 +0800

    installed nyxb ui library

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,15 +1,16 @@
 "use client"
 
+import { Button } from "@/components/ui/button";
 import { useCounterStore } from "@/stores/counter-store";
 
 export default function Home() {
   const counter = useCounterStore((state) => state);
 
   return (
-    <div className="flex gap-4">
-      <button onClick={counter.decrement}>minus 1</button>
+    <div className="flex gap-4 p-4">
+      <Button onClick={counter.decrement}>minus 1</Button>
       <h1>{counter.count}</h1>
-      <button onClick={counter.increment}>add 1</button>
+      <Button onClick={counter.increment}>add 1</Button>
     </div>
   );
 }
