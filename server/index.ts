8e3deb162a6fbf4f031fcf311c026aed89e27c57
Jasper <jasper.apos@gmail.com>
2024-10-24 19:39:23 +0800

    installed bcrypt and cors dependencies

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import { config } from "./utils/config.utils";
+import userRouter from "./routes/user.routes";
+import cors from "cors"
 
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-})
+app.use(cors({
+  origin: "http://localhost:3001",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}))
+app.use(express.json());
+app.use(userRouter);
 
 app.listen(config.PORT, () => {
   console.log(`Server is running in port ${config.PORT}`);
