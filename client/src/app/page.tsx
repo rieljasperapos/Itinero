3b859a00b715563a8dc703337188f9336402bc2f
Jim5e <jamesterenceclarit@gmail.com>
2024-12-01 21:27:11 +0800

    added sample itinerary

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,13 +2,20 @@
 
 import { useCounterStore } from "@/stores/counter-store";
 import Dashboard from "./dashboard";
+import ItineraryDetails from "@/components/itinerary_details";
 
 export default function Home() {
   const counter = useCounterStore((state) => state);
 
   return (
-    <div className="flex gap-4">
+    <div className="flex">
       <Dashboard />
+      <ItineraryDetails 
+        title="Sample Title" 
+        dateStart="2023-10-01" 
+        dateEnd="2023-10-02"
+        collaborators={3}
+      />
     </div>
   );
 }
