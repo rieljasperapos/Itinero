5b96a7bed7871defc86b4f9a23361c967da335e1
Jasper <jasper.apos@gmail.com>
2024-12-05 17:14:58 +0800

    feat[sidebar]: implemented sidebar navigations and fix calendar UI

--- client/src/app/calendar/page.tsx
+++ client/src/app/calendar/page.tsx
@@ -1,10 +1,26 @@
 "use client";
+import { AppSidebar } from "../../components/AppSidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/breadcrumb";
+import { Separator } from "@/components/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/sidebar";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import { useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { MapPin, CalendarDays, Clock } from "lucide-react";
+import "./_lib/custom-calendar.css";
 
 const Calendar = () => {
   const [activities, setActivities] = useState([]);
@@ -14,22 +30,26 @@ const Calendar = () => {
     const fetchActivities = async () => {
       if (session?.user.accessToken) {
         try {
-          const response = await axios.get("http://localhost:3000/itineraries", {
-            headers: {
-              Authorization: `Bearer ${session?.user.accessToken}`,
-            },
-          });
+          const response = await axios.get(
+            "http://localhost:3000/itineraries",
+            {
+              headers: {
+                Authorization: `Bearer ${session?.user.accessToken}`,
+              },
+            }
+          );
           console.log("Fetched activities:", response.data);
 
           // Map the response data to FullCalendar event format
-          const fetchedActivities = response.data.data.flatMap((itinerary: any) =>
-            itinerary.activities.map((activity: any) => ({
-              title: activity.activityName,
-              start: activity.startTime,
-              end: activity.endTime,
-              location: activity.locationName, // Optional: extra data if needed
-              itinerary: itinerary.title,
-            }))
+          const fetchedActivities = response.data.data.flatMap(
+            (itinerary: any) =>
+              itinerary.activities.map((activity: any) => ({
+                title: activity.activityName,
+                start: activity.startTime,
+                end: activity.endTime,
+                location: activity.locationName, // Optional: extra data if needed
+                itinerary: itinerary.title,
+              }))
           );
 
           setActivities(fetchedActivities);
@@ -52,53 +72,71 @@ const Calendar = () => {
   }
 
   return (
-    <div className="p-8">
-      <div className="border-b py-2">
-        <h1 className="heading">Calendar</h1>
-      </div>
-      <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
-        <FullCalendar
-          timeZone="Asia/Manila"
-          plugins={[dayGridPlugin]}
-          initialView="dayGridMonth"
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,dayGridWeek,dayGridYear",
-          }}
-          initialDate={new Date()}
-          editable={true}
-          selectable={true}
-          events={activities} // Use the state here
-          eventContent={(eventInfo) => {
-            const startTime = formatTime(eventInfo.event.start);
-            const endTime = formatTime(eventInfo.event.end);
-
-            return (
-              <div className="rounded-lg p-2 bg-gray-100 ease-in-out truncate flex flex-col gap-2">
-                <div className="text-xs md:text-sm mt-1 italic font-bold border-b border-gray-300 p-2">
-                  {eventInfo.event.extendedProps.itinerary}
-                </div>
-                <div className="flex items-center">
-                  <CalendarDays className="h-4 w-4 mr-1" />
-                  <span className="text-sm md:text-base font-medium">
-                    {eventInfo.event.title}
-                  </span>
-                </div>
-                <div className="flex items-center mt-1">
-                  <MapPin className="h-4 w-4 mr-1" />
-                  <span className="text-xs md:text-sm">{eventInfo.event.extendedProps.location}</span>
-                </div>
-                <div className="flex items-center text-xs md:text-sm mt-1">
-                  <Clock className="h-4 w-4 mr-1" />
-                  {startTime} - {endTime}
-                </div>
-              </div>
-            );
-          }}
-        />
-      </div>
-    </div>
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbPage>Calendar</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          <div className="">
+            <div className="max-w-7xl mx-auto p-4">
+              <FullCalendar
+                timeZone="Asia/Manila"
+                plugins={[dayGridPlugin]}
+                initialView="dayGridMonth"
+                headerToolbar={{
+                  left: "prev,next today",
+                  center: "title",
+                  right: "dayGridMonth,dayGridWeek,dayGridYear",
+                }}
+                initialDate={new Date()}
+                editable={true}
+                selectable={true}
+                events={activities} // Use the state here
+                eventContent={(eventInfo) => {
+                  const startTime = formatTime(eventInfo.event.start);
+                  const endTime = formatTime(eventInfo.event.end);
+                  return (
+                    <div className="rounded-lg p-2 ease-in-out truncate flex flex-col gap-1">
+                      <div className="text-xs mt-1 italic bg-gray-100 font-bold border-gray-300 p-2">
+                        {eventInfo.event.extendedProps.itinerary}
+                      </div>
+                      <div className="flex items-center">
+                        <CalendarDays className="h-4 w-4 mr-1" />
+                        <span className="text-sm font-medium">
+                          {eventInfo.event.title}
+                        </span>
+                      </div>
+                      <div className="flex items-center mt-1">
+                        <MapPin className="h-4 w-4 mr-1" />
+                        <span className="text-xs">
+                          {eventInfo.event.extendedProps.location}
+                        </span>
+                      </div>
+                      <div className="flex items-center text-xs mt-1">
+                        <Clock className="h-4 w-4 mr-1" />
+                        {startTime} - {endTime}
+                      </div>
+                    </div>
+                  );
+                }}
+              />
+            </div>
+          </div>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
   );
 };
 
