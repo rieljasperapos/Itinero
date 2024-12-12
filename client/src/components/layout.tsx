77a473b10589cbfaf6990aec91241756ad3cad79
Jasper <jasper.apos@gmail.com>
2024-12-12 16:43:20 +0800

    feat[layout]: made sidebar callable to each component to have DRY (Dont Repeat Yourself) design

---
+++ client/src/components/layout.tsx
@@ -0,0 +1,43 @@
+"use client";
+import { AppSidebar } from "./AppSidebar";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from "@/components/breadcrumb";
+import { Separator } from "@/components/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/sidebar";
+import { LayoutProps } from "@/types/layout-type";
+
+const Layout = ({ children, breadcrumb }: LayoutProps) => {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{breadcrumb}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
+          {children}
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+};
+
+export default Layout;
