a7633df78a3198b1911bd1bb8d4379c0dd75563f
jasper <jasper.apos@gmail.com>
2024-12-18 00:44:54 +0800

    remove leaflet link

--- client/src/app/map/_components/map.tsx
+++ client/src/app/map/_components/map.tsx
@@ -8,14 +8,14 @@ export default function MyMap(props: any) {
 
   return (
     <div className="h-[75vh]">
-    <MapContainer center={position} zoom={zoom} scrollWheelZoom={true} style={{ height: "100%", width: "100%", borderRadius: "30px", zIndex: 5 }}>
+    <MapContainer attributionControl={false} center={position} zoom={zoom} scrollWheelZoom={true} style={{ height: "100%", width: "100%", borderRadius: "30px", zIndex: 5 }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Marker position={position}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          The location of the activity is here.
         </Popup>
       </Marker>
     </MapContainer>
